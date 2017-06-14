a3898163f698bb77685b37a06c3ce305aaebccf6
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-06-14 13:42:21 -0400

    working with axios and github api

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,47 +1,105 @@
+
 var React = require('react');
+var PropTypes = require('prop-types');
+var api = require('../utils/api');
+
+function SelectLanguage (props) {
+   var languages = ['All Tools','JS Frameworks', 'JS Preprocessors', 'Build Automations','Text Editors', 'UI Frameworks', 'Templating', 'Browser Refreshing', 'CSS Preprocessors', 'Style Guides', 'Version Control', 'Package Mangers', 'Performance','Code Linting','Testing','Servern Side JS'];
+
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+function RepoGrid (props) {
+  return (
+    <ul className='popular-list'>
+      {props.repos.map(function (repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={'Avatar for ' + repo.owner.login}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
-      selectedLanguage: 'All Too',
+      selectedLanguage: 'All',
+      repos: null,
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  componentDidMount() {
+   this.updateLanguage(this.state)
   }
   updateLanguage(lang) {
     this.setState(function () {
       return {
         selectedLanguage: lang,
+        repos: null
       }
     });
+
+    api.fetchPopularRepos(lang)
+      .then(function (repos) {
+        this.setState(function () {
+          return {
+            repos: repos
+          }
+        });
+      }.bind(this));
   }
   render() {
-    var languages = ['All Tools','JS Frameworks', 'JS Preprocessors', 'Build Automations','Text Editors', 'UI Frameworks', 'Templating', 'Browser Refreshing', 'CSS Preprocessors', 'Style Guides', 'Version Control', 'Package Mangers', 'Performance','Code Linting','Testing','Servern Side JS'];
-   
-
     return (
       <div>
-      <h1>The Front End Eco-System</h1>
-
-        <ul className='uk-nav uk-nav-side'>
-          {languages.map(function (lang) {
-            return (
-              <li
-               
-                className={lang === this.state.selectedLanguage ? 'uk-active' : null}
-             
-                onClick={this.updateLanguage.bind(null, lang)}
-                key={lang}>
-                  <a >{lang}</a>
-              </li>
-            )
-          }, this)}
-        </ul>
-              <p>This is placeholder for  {this.state.selectedLanguage} blurb to be outputted for better description</p>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+        {!this.state.repos
+          ? <p>LOADING!</p>
+          : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
 }
 
 module.exports = Popular;
+
--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+require('./bulma.css');
 var App = require('./components/App');
 
 ReactDOM.render(
