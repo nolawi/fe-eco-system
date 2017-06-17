96e3476307cca92ba578ab46954f53c87cd1b4a1
Nolawi Petros <nolawipetros@Nolawis-MacBook-Pro.local>
2017-06-17 16:09:56 -0400

    start of nav

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,22 @@
 var React = require('react');
 var Popular = require('./popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./Nav');
+var Home = require('./Home');
+Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+    	<Router>
+	      <div className='container'>
+	        <Route path='/' component={Popular} />
+	        <Popular />
+	      </div>
+        </Router>
     )
   }
 }
